c6b610f05143f29829282737e1a4c17cbecb72f2
SDGLBL <lijieisguge@gmail.com>
2023-03-28 11:43:38 +0800

    :art: Add style.toml and format code

--- lua/nvim-sync/api.lua
+++ lua/nvim-sync/api.lua
@@ -1,61 +1,63 @@
 local M = {}
 
-local utils = require("nvim-sync.utils")
+local utils = require "nvim-sync.utils"
 local config = require("nvim-sync.config").config
-local job = require("plenary.job")
+local job = require "plenary.job"
 
 local function check_file_path(file_path)
-	if not utils.is_valid(file_path) then
-		vim.notify("Invalid file path" .. file_path, vim.log.levels.TRACE)
-		return false
-	end
+  if not utils.is_valid(file_path) then
+    vim.notify("Invalid file path" .. file_path, vim.log.levels.TRACE)
+    return false
+  end
 
-	return true
+  return true
 end
 
 -- exec upload or download a file to a remote server using given sync executable
 -- @param action: string (upload|download)
 -- @param filepath: string
 local function exec(action, file_path)
-	if not check_file_path(file_path) then
-		return
-	end
+  if not check_file_path(file_path) then
+    return
+  end
 
-	local project_root = utils.get_project_root()
+  local project_root = utils.get_project_root()
 
-	if not project_root then
-		vim.notify("Project root not found", vim.log.levels.TRACE)
-		return
-	end
+  if not project_root then
+    vim.notify("Project root not found", vim.log.levels.TRACE)
+    return
+  end
 
-	local filename = vim.fn.fnamemodify(file_path, ":t")
-	local file_relative_path = utils.get_relative_path_to(file_path, project_root)
+  local filename = vim.fn.fnamemodify(file_path, ":t")
+  local file_relative_path = utils.get_relative_path_to(file_path, project_root)
 
-	local sync_exe_path = ""
+  local sync_exe_path = ""
 
-	if config.sync_exe_path == nil then
-		sync_exe_path = require("nvim-sync.utils").find_exec_file(config.sync_exe_filename)
-	end
+  if config.sync_exe_path == nil then
+    sync_exe_path = utils.find_exec_file(config.sync_exe_filename)
+  end
 
-	if not sync_exe_path then
-		vim.notify("Nvim-sync: No executable sync file found", vim.log.levels.INFO)
-		return
-	end
+  if not sync_exe_path then
+    vim.notify("Nvim-sync: No executable sync file found", vim.log.levels.INFO)
+    return
+  end
 
-	job:new({
-		command = sync_exe_path,
-		args = { action, file_relative_path, filename },
-		on_start = function()
-			print("Nvim-sync: " .. action .. "file " .. file_path .. " started")
-		end,
-		on_exit = function(_, exit_code)
-			if exit_code ~= 0 then
-				print("Nvim-sync: " .. action .. "file " .. file_path .. " failed")
-			else
-				print("Nvim-sync: " .. action .. "file " .. file_path .. " success")
-			end
-		end,
-	}):start()
+  job
+    :new({
+      command = sync_exe_path,
+      args = { action, file_relative_path, filename },
+      on_start = function()
+        print("Nvim-sync: " .. action .. "file " .. file_path .. " started")
+      end,
+      on_exit = function(_, exit_code)
+        if exit_code ~= 0 then
+          print("Nvim-sync: " .. action .. "file " .. file_path .. " failed")
+        else
+          print("Nvim-sync: " .. action .. "file " .. file_path .. " success")
+        end
+      end,
+    })
+    :start()
 end
 
 -- upload a file to a remote server
@@ -63,8 +65,8 @@ end
 -- @param filepath: string
 -- @return boolean
 M.upload = function()
-	local filepath = utils.get_file_path()
-	exec("upload", filepath)
+  local filepath = utils.get_file_path()
+  exec("upload", filepath)
 end
 
 -- download a file from remote server
@@ -72,20 +74,20 @@ end
 -- @param filepath: string
 -- @return boolean
 M.download = function()
-	local filepath = utils.get_file_path()
-	exec("download", filepath)
+  local filepath = utils.get_file_path()
+  exec("download", filepath)
 end
 
 M.init = function()
-	vim.api.nvim_create_user_command("SyncUpload", M.upload, {
-		range = false,
-		nargs = "*",
-	})
+  vim.api.nvim_create_user_command("SyncUpload", M.upload, {
+    range = false,
+    nargs = "*",
+  })
 
-	vim.api.nvim_create_user_command("SyncDownload", M.download, {
-		range = false,
-		nargs = "*",
-	})
+  vim.api.nvim_create_user_command("SyncDownload", M.download, {
+    range = false,
+    nargs = "*",
+  })
 end
 
 return M
--- lua/nvim-sync/utils.lua
+++ lua/nvim-sync/utils.lua
@@ -1,166 +1,166 @@
 local M = {}
 
 local config = require("nvim-sync.config").config
-local ppath = require("plenary.path")
-local glob = require("nvim-sync.globtopattern")
+local ppath = require "plenary.path"
+local glob = require "nvim-sync.globtopattern"
 local uv = vim.loop
 
 local function find_lsp_root()
-	local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-	local clients = vim.lsp.buf_get_clients()
-	if next(clients) == nil then
-		return nil
-	end
+  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+  local clients = vim.lsp.buf_get_clients()
+  if next(clients) == nil then
+    return nil
+  end
 
-	for _, client in pairs(clients) do
-		local filetypes = client.config.filetypes
-		if filetypes and vim.tbl_contains(filetypes, buf_ft) then
-			if not vim.tbl_contains(config.ignore_lsp, client.name) then
-				return client.config.root_dir, client.name
-			end
-		end
-	end
+  for _, client in pairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.tbl_contains(filetypes, buf_ft) then
+      if not vim.tbl_contains(config.ignore_lsp, client.name) then
+        return client.config.root_dir, client.name
+      end
+    end
+  end
 end
 
 -- find_pattern_root Finds the root directory of the project using the patterns
 -- defined in config.lua
 -- @return string
 local function find_pattern_root()
-	local search_dir = vim.fn.expand("%:p:h", true)
-	if vim.fn.has("win32") > 0 then
-		search_dir = search_dir:gsub("\\", "/")
-	end
+  local search_dir = vim.fn.expand("%:p:h", true)
+  if vim.fn.has "win32" > 0 then
+    search_dir = search_dir:gsub("\\", "/")
+  end
 
-	local last_dir_cache = ""
-	local curr_dir_cache = {}
+  local last_dir_cache = ""
+  local curr_dir_cache = {}
 
-	local function get_parent(path)
-		path = path:match("^(.*)/")
-		if path == "" then
-			path = "/"
-		end
-		return path
-	end
+  local function get_parent(path)
+    path = path:match "^(.*)/"
+    if path == "" then
+      path = "/"
+    end
+    return path
+  end
 
-	local function get_files(file_dir)
-		last_dir_cache = file_dir
-		curr_dir_cache = {}
+  local function get_files(file_dir)
+    last_dir_cache = file_dir
+    curr_dir_cache = {}
 
-		local dir = uv.fs_scandir(file_dir)
-		if dir == nil then
-			return
-		end
+    local dir = uv.fs_scandir(file_dir)
+    if dir == nil then
+      return
+    end
 
-		while true do
-			local file = uv.fs_scandir_next(dir)
-			if file == nil then
-				return
-			end
+    while true do
+      local file = uv.fs_scandir_next(dir)
+      if file == nil then
+        return
+      end
 
-			table.insert(curr_dir_cache, file)
-		end
-	end
+      table.insert(curr_dir_cache, file)
+    end
+  end
 
-	local function is(dir, identifier)
-		dir = dir:match(".*/(.*)")
-		return dir == identifier
-	end
+  local function is(dir, identifier)
+    dir = dir:match ".*/(.*)"
+    return dir == identifier
+  end
 
-	local function sub(dir, identifier)
-		local path = get_parent(dir)
-		while true do
-			if is(path, identifier) then
-				return true
-			end
-			local current = path
-			path = get_parent(path)
-			if current == path then
-				return false
-			end
-		end
-	end
+  local function sub(dir, identifier)
+    local path = get_parent(dir)
+    while true do
+      if is(path, identifier) then
+        return true
+      end
+      local current = path
+      path = get_parent(path)
+      if current == path then
+        return false
+      end
+    end
+  end
 
-	local function child(dir, identifier)
-		local path = get_parent(dir)
-		return is(path, identifier)
-	end
+  local function child(dir, identifier)
+    local path = get_parent(dir)
+    return is(path, identifier)
+  end
 
-	local function has(dir, identifier)
-		if last_dir_cache ~= dir then
-			get_files(dir)
-		end
-		local pattern = glob.globtopattern(identifier)
-		for _, file in ipairs(curr_dir_cache) do
-			if file:match(pattern) ~= nil then
-				return true
-			end
-		end
-		return false
-	end
+  local function has(dir, identifier)
+    if last_dir_cache ~= dir then
+      get_files(dir)
+    end
+    local pattern = glob.globtopattern(identifier)
+    for _, file in ipairs(curr_dir_cache) do
+      if file:match(pattern) ~= nil then
+        return true
+      end
+    end
+    return false
+  end
 
-	local function match(dir, pattern)
-		local first_char = pattern:sub(1, 1)
-		if first_char == "=" then
-			return is(dir, pattern:sub(2))
-		elseif first_char == "^" then
-			return sub(dir, pattern:sub(2))
-		elseif first_char == ">" then
-			return child(dir, pattern:sub(2))
-		else
-			return has(dir, pattern)
-		end
-	end
+  local function match(dir, pattern)
+    local first_char = pattern:sub(1, 1)
+    if first_char == "=" then
+      return is(dir, pattern:sub(2))
+    elseif first_char == "^" then
+      return sub(dir, pattern:sub(2))
+    elseif first_char == ">" then
+      return child(dir, pattern:sub(2))
+    else
+      return has(dir, pattern)
+    end
+  end
 
-	-- breadth-first search
-	while true do
-		for _, pattern in ipairs(config.patterns) do
-			local exclude = false
-			if pattern:sub(1, 1) == "!" then
-				exclude = true
-				pattern = pattern:sub(2)
-			end
-			if match(search_dir, pattern) then
-				if exclude then
-					break
-				else
-					return search_dir, "pattern " .. pattern
-				end
-			end
-		end
+  -- breadth-first search
+  while true do
+    for _, pattern in ipairs(config.patterns) do
+      local exclude = false
+      if pattern:sub(1, 1) == "!" then
+        exclude = true
+        pattern = pattern:sub(2)
+      end
+      if match(search_dir, pattern) then
+        if exclude then
+          break
+        else
+          return search_dir, "pattern " .. pattern
+        end
+      end
+    end
 
-		local parent = get_parent(search_dir)
-		if parent == search_dir or parent == nil then
-			return nil
-		end
+    local parent = get_parent(search_dir)
+    if parent == search_dir or parent == nil then
+      return nil
+    end
 
-		search_dir = parent
-	end
+    search_dir = parent
+  end
 end
 
 -- get_file_path Get the path of the current file
 -- @return string
 M.get_file_path = function()
-	return vim.fn.expand("%:p")
+  return vim.fn.expand "%:p"
 end
 
 -- get_project_root Get the root directory of the project
 -- @return string
 -- @usage local root_dir = utils.get_project_root()
 M.get_project_root = function()
-	-- returns project root, as well as method
-	for _, detection_method in ipairs(config.detection_methods) do
-		if detection_method == "lsp" then
-			local root, lsp_name = find_lsp_root()
-			if root ~= nil then
-				return root, '"' .. lsp_name .. '"' .. " lsp"
-			end
-		elseif detection_method == "pattern" then
-			local root, method = find_pattern_root()
-			if root ~= nil then
-				return root, method
-			end
-		end
-	end
+  -- returns project root, as well as method
+  for _, detection_method in ipairs(config.detection_methods) do
+    if detection_method == "lsp" then
+      local root, lsp_name = find_lsp_root()
+      if root ~= nil then
+        return root, '"' .. lsp_name .. '"' .. " lsp"
+      end
+    elseif detection_method == "pattern" then
+      local root, method = find_pattern_root()
+      if root ~= nil then
+        return root, method
+      end
+    end
+  end
 end
 
 -- find_exec_file Finds the executable file in the project root path
@@ -168,16 +168,16 @@ end
 -- @return string
 -- @usage local exec_file_path = utils.find_exec_file(exec_file_name)
 M.find_exec_file = function(exec_file_name)
-	local root_dir = M.get_project_root()
-	local exec_file_path = root_dir .. "/" .. exec_file_name
+  local root_dir = M.get_project_root()
+  local exec_file_path = root_dir .. "/" .. exec_file_name
 
-	if M.is_executable(exec_file_path) then
-		return exec_file_path
-	end
+  if M.is_executable(exec_file_path) then
+    return exec_file_path
+  end
 
-	if vim.fn.executable(exec_file_name) == 1 then
-		return exec_file_path
-	end
+  if vim.fn.executable(exec_file_name) == 1 then
+    return exec_file_path
+  end
 end
 
 -- get_relative_path_to Get the relative path of a file from path
@@ -189,10 +189,10 @@ end
 --  get_relative_path_to('/home/user/bin/my-script.sh', '/home')
 --  Returns user/bin
 M.get_relative_path_to = function(file_path, path)
-	vim.cmd("lcd " .. path)
-	local rl = vim.fn.fnamemodify(file_path, ":.:h")
-	vim.cmd("lcd -")
-	return rl
+  vim.cmd("lcd " .. path)
+  local rl = vim.fn.fnamemodify(file_path, ":.:h")
+  vim.cmd "lcd -"
+  return rl
 end
 
 -- is_executable Checks if a file is executable by the current user
@@ -200,7 +200,7 @@ end
 -- @return boolean
 -- @usage local is_executable = utils.is_executable(path)
 M.is_executable = function(path)
-	return path and os.execute("test -x " .. path) == 0
+  return path and os.execute("test -x " .. path) == 0
 end
 
 -- is_symlink Checks if a file is a symbolic link
@@ -208,7 +208,7 @@ end
 -- @return boolean
 -- @usage local is_symlink = utils.is_symlink(path)
 M.is_symlink = function(path)
-	return path and vim.fn.getftype(path) == "link"
+  return path and vim.fn.getftype(path) == "link"
 end
 
 -- is_valid checks if the given path is a valid file
@@ -216,7 +216,10 @@ end
 -- @return boolean
 -- @usage local is_valid = utils.is_valid(path)
 M.is_valid = function(path)
-	return path and not M.is_executable(path) and not M.is_symlink(path) and vim.fn.filereadable(path) == 1
+  return path
+    and not M.is_executable(path)
+    and not M.is_symlink(path)
+    and vim.fn.filereadable(path) == 1
 end
 
 return M
